229a31bc0d63282608aa3a67525cca162b174dd7
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-07-15 13:55:28 -0400

    Minor tweaks to time series shapefile-based summary stat maker.

--- time_series/summary_stats_box.py
+++ time_series/summary_stats_box.py
@@ -11,10 +11,11 @@ import fiona
 import csv
 import numpy as np
 import glob
+import ntpath
 
 # Set workspaces etc
-workspace = '/media/arthur/Windows/LinuxShare/data/MCD43A3/hdf/2019/h16v02/wsa/wgs84/'
-product_name = 'mcd43a3'
+workspace = '/media/arthur/Windows/LinuxShare/S2/albedo/T22WEV/wsa/wgs84/'
+product_name = 's2'
 
 with fiona.open('/home/arthur/Dropbox/projects/greenland/sensor_intercompare/intersection_006013_T22WEV_h16v02_wgs84.shp',
                 'r') as clip_shp:
@@ -23,7 +24,7 @@ with fiona.open('/home/arthur/Dropbox/projects/greenland/sensor_intercompare/int
 
 # List to hold outputs
 stats_list = []
-csv_header = ['mean', 'sd_dev']
+csv_header = ['product', 'mean', 'sd_dev']
 
 for tif in glob.glob(workspace + '*.tif'):
     # Loop over all tifs in indir, clip each using clip_shp, and then calculate mean an sd, append to list
@@ -33,13 +34,18 @@ for tif in glob.glob(workspace + '*.tif'):
 
     masked_clipped_img = np.ma.masked_array(clipped_img, clipped_img == 32767)
 
-    mean = masked_clipped_img.mean() * 0.001
-    std = masked_clipped_img.std() * 0.001
+    if 'mcd' in product_name or 'MCD' in product_name:
+        mean = masked_clipped_img.mean() * 0.001
+        std = masked_clipped_img.std() * 0.001
+    elif 'lc' in product_name or 's2' in product_name or 'LC' in product_name or 'S2' in product_name:
+        mean = masked_clipped_img.mean() * 0.0001
+        std = masked_clipped_img.std() * 0.0001
+    tif_name = ntpath.basename(tif)
 
-    stats_list.append((mean, std))
+    stats_list.append((tif_name, mean, std))
 
     # Write stats_list to csv
-    with open(workspace + product_name + 'stats.csv', 'w') as csv_file:
+    with open(workspace + product_name + '_stats.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(csv_header)
         writer.writerows(stats_list)
