fef46c799548c1b3df380010548d0399cba08ef1
Arthur <arthur.elmes@gmail.com>
2020-07-21 15:29:09 -0400

    Separated hdf_to_tif converter into mcd43a3 and mcd43a2 versions. Currently mcd43a2 version only convers SZA.

---
+++ data_manage/convert/hdf_to_tif_mcd43a2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+in_dir=$1 
+out_dir=$2
+
+srs_str=$'\x22+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs\x22'
+
+
+if [ ! -d ${out_dir} ]; then
+    mkdir ${out_dir}
+fi
+
+if [ ! -d ${out_dir}/sza/ ]; then
+    mkdir ${out_dir}/sza/
+fi
+
+
+for hdf in $in_dir/*.hdf
+do
+    #echo $hdf
+    filename=$(basename -- $hdf)
+    extension="${filename##*.}"
+    filename_bare="${filename%.*}"
+    #gdal_translate "-a_srs ${srs_str} -a_nodata 255 -of GTiff \x22HDF4_EOS:EOS_GRID:${in_dir}${filename}:MOD_Grid_BRDF:BRDF_Albedo_LocalSolarNoon\x22 ${out_dir}/sza/${filename_bare}_sza.tif"
+    #gdal_translate "-a_srs ${srs_str} -a_nodata 255 -of GTiff 'HDF4_EOS:EOS_GRID:${in_dir}${filename}:MOD_Grid_BRDF:BRDF_Albedo_LocalSolarNoon' ${out_dir}/sza/${filename_bare}_sza.tif"
+    gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"  -a_nodata 255 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}${filename}'"':MOD_Grid_BRDF:BRDF_Albedo_LocalSolarNoon ${out_dir}/sza/${filename_bare}_sza.tif
+
+done
--- data_manage/convert/hdf_to_tif_mcd43a3.sh
+++ data_manage/convert/hdf_to_tif_mcd43a3.sh
@@ -23,6 +23,7 @@ if [ ! -d ${out_dir}/bsa/ ]; then
     mkdir ${out_dir}/bsa/
 fi
 
+
 for hdf in $in_dir/*.hdf
 do
     echo $hdf
@@ -36,8 +37,9 @@ do
     # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_Band5 ${out_dir}/${filename_bare}_wsa_b5.tif
     # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_Band6 ${out_dir}/${filename_bare}_wsa_b6.tif
     # gdal_translate -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_Band7 ${out_dir}/${filename_bare}_wsa_b7.tif
-    gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 32767 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_shortwave ${out_dir}/wsa/${filename_bare}_wsa_shortwave.tif
-    gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 32767 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_BSA_shortwave ${out_dir}/bsa/${filename_bare}_bsa_shortwave.tif
-    gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 255 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:BRDF_Albedo_Band_Mandatory_Quality_shortwave ${out_dir}/qa/${filename_bare}_qa_shortwave.tif
-    # echo $srs_str		
+     gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 32767 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_WSA_shortwave ${out_dir}/wsa/${filename_bare}_wsa_shortwave.tif
+     gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 32767 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:Albedo_BSA_shortwave ${out_dir}/bsa/${filename_bare}_bsa_shortwave.tif
+     gdal_translate -a_srs "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" -a_nodata 255 -of GTiff HDF4_EOS:EOS_GRID:'"'${in_dir}/${filename}'"':MOD_Grid_BRDF:BRDF_Albedo_Band_Mandatory_Quality_shortwave ${out_dir}/qa/${filename_bare}_qa_shortwave.tif
+
+
 done
