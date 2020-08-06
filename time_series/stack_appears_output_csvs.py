96c211fe7d6d2bd046fabf6ed635eff607ef14a0
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-08-06 15:16:00 -0400

    Updated appers_time-series_graphs.py to include separate line for 2020.

--- time_series/stack_appears_output_csvs.py
+++ time_series/stack_appears_output_csvs.py
@@ -5,13 +5,13 @@ Date: 2020-06-09"""
 import os, glob
 import pandas as pd
 
-workspace = '/home/arthur/Dropbox/projects/greenland/transect_points/appears/'
+workspace = '/home/arthur/Dropbox/projects/greenland/aoi_albedo_time_series/appears/west_coast_add_2020/'
 os.chdir(workspace)
 
-csv_file_names = glob.glob('russel-1-with-parallel*.csv')
+csv_file_names = glob.glob('west*.csv')
 
 combined_csv = pd.concat([pd.read_csv(f) for f in csv_file_names])
 print(combined_csv.shape)
-csv_name_all = 'russel-1-with-parallel-combined-MCD43A3-006-results.csv'
+csv_name_all = 'west-coast-combined-MCD43A3-006-results-2000-2020.csv'
 combined_csv.to_csv(csv_name_all, index=False)
 
