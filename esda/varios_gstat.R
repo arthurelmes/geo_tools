32711272b03ae5b9ca551982000499749e711465
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-08-14 08:27:24 -0400

    Minor update to R varios script.

--- esda/varios_gstat.R
+++ esda/varios_gstat.R
@@ -18,41 +18,42 @@ file_names <- dir(wd_path, pattern=".tif")
 plot_list = list()
 
 for(i in 1:length(file_names)){
-  print(file_names[i])
-  tif_raster <- raster(file_names[i])
-  tif_raster_variable <- tools::file_path_sans_ext(file_names[i])
-  print(tif_raster_variable)
+  try({
+    print(file_names[i])
+    tif_raster <- raster(file_names[i])
+    tif_raster_variable <- tools::file_path_sans_ext(file_names[i])
+    print(tif_raster_variable)
   
-  print("masking raster")  
-  # clip the raster to the shapefile and its extent (trim)
-  masked_raster <- mask(tif_raster, clip_file_name)
-  try(non_na_raster <- trim(masked_raster))
-  
-  # construct a SpatailPointsDataFrame from a sample of the raster, because using all points, as in the commented out line,
-  # is waaaay to resource intensive
-  #point_data <- as(non_na_raster, 'SpatialPointsDataFrame')
-  
-  print("sampling raster")
-  point_data <- as.data.frame(sampleRandom(x=non_na_raster, size = 500, na.rm = TRUE, ext = clip_file_name, xy = TRUE))
-  xy <- cbind(point_data[1], point_data[2])
-  
-  print("masking spatialpointsdf")
-  point_data <- SpatialPointsDataFrame(xy, point_data)
-  
-  # plot the layers to check
-  #plot(non_na_raster)
-  #plot(point_data, add=TRUE)
-  
-  f <- paste(tif_raster_variable, "~ 1")
-  h <- as.formula(f)
-  
-  print("running variogram")
-  try(gstat_variogram <- variogram(h, data = point_data))
-  print("ok the variogram is created")
-  plot_title = "Variogram for "
-  p = plot(gstat_variogram, main = plot_title, cex.main=0.5)
-  plot_list[[i]] = p
-  
+    print("masking raster")  
+    # clip the raster to the shapefile and its extent (trim)
+    masked_raster <- mask(tif_raster, clip_file_name)
+    non_na_raster <- trim(masked_raster)
+    
+    # construct a SpatailPointsDataFrame from a sample of the raster, because using all points, as in the commented out line,
+    # is waaaay to resource intensive
+    #point_data <- as(non_na_raster, 'SpatialPointsDataFrame')
+    
+    print("sampling raster")
+    point_data <- as.data.frame(sampleRandom(x=non_na_raster, size = 500, na.rm = TRUE, ext = clip_file_name, xy = TRUE))
+    xy <- cbind(point_data[1], point_data[2])
+    
+    print("masking spatialpointsdf")
+    point_data <- SpatialPointsDataFrame(xy, point_data)
+    
+    # plot the layers to check
+    #plot(non_na_raster)
+    #plot(point_data, add=TRUE)
+    
+    f <- paste(tif_raster_variable, "~ 1")
+    h <- as.formula(f)
+    
+    print("running variogram")
+    gstat_variogram <- variogram(h, data = point_data)
+    print("ok the variogram is created")
+    plot_title = "Variogram for "
+    p = plot(gstat_variogram, main = plot_title, cex.main=0.5)
+    plot_list[[i]] = p
+  })
 }
 
 for(i in 1:length(plot_list)){
