dbc361d338e28b340c7669cfbe9a5f542d1fb4b7
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-08-17 12:26:32 -0400

    Added bash-only csv merger, and updated existing python-based csv merger.

---
+++ data_manage/merge_csvs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Credit: user Hastur, https://stackoverflow.com/questions/24641948/merging-csv-files-appending-instead-of-merging
+
+in_dir=$1
+out_file_name=$2      # Fix the output name
+i=0                                       # Reset a counter
+for filename in ./${in_dir}/*.csv; do 
+ if [ "$filename"  != "$out_file_name" ] ;      # Avoid recursion 
+ then 
+   if [[ $i -eq 0 ]] ; then 
+      head -1  "$filename" >   "$out_file_name" # Copy header if it is the first file
+   fi
+   tail -n +2  "$filename" >>  "$out_file_name" # Append from the 2nd line each file
+   i=$(( $i + 1 ))                            # Increase the counter
+ fi
+done
--- time_series/stack_appears_output_csvs.py
+++ time_series/stack_appears_output_csvs.py
@@ -1,17 +1,17 @@
 """Combine a series of appears results into one big csv (due to appears request size limit)
-Author: Arthur ELmes
+Author: Arthur Elmes
 Date: 2020-06-09"""
 
 import os, glob
 import pandas as pd
 
-workspace = '/home/arthur/Dropbox/projects/greenland/aoi_albedo_time_series/appears/west_coast_add_2020/'
+workspace = '/home/arthur/Dropbox/projects/greenland/blue_sky_variables/SZA/h15v03/'
 os.chdir(workspace)
 
-csv_file_names = glob.glob('west*.csv')
+csv_file_names = glob.glob('*.csv')
 
 combined_csv = pd.concat([pd.read_csv(f) for f in csv_file_names])
 print(combined_csv.shape)
-csv_name_all = 'west-coast-combined-MCD43A3-006-results-2000-2020.csv'
+csv_name_all = 'h15v03_AOD_2000_2020.csv'
 combined_csv.to_csv(csv_name_all, index=False)
 
