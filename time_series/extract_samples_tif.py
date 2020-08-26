d7003bae3dc6ef3da3b73e599f13335ddad8e275
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-08-26 09:39:45 -0400

    Fixed extract_samples_tif.py so that it creates a properly formatted csv output. Plotting function does not yet work.

--- time_series/extract_samples_tif.py
+++ time_series/extract_samples_tif.py
@@ -45,45 +45,51 @@ def make_prod_list(in_dir, prdct, year, day):
     return t_file_list
 
 
-def extract_pixel_value(site, t_file_day):
+def extract_pixel_values(sites_dict, t_file_day):
     # Open tifs with rasterio
-    #print('Opening: ' + t_file_day)
 
     with rio.open(t_file_day,
                   'r',
                   driver='GTiff') as tif:
 
-        #print("lon/lat are: ")
-        lon, lat = (float(site[1][1]), float(site[1][0]))
-        #print((lon, lat))
-
-        #print("col/row are: ")
-        col, row = tif.index(lon, lat)
-        #print((col, row))
+        rc_list = []
+        for site in sites_dict.items():
+            col, row = tif.index(float(site[1][1]), float(site[1][0]))
+            rc_list.append((col, row))
 
         tif_np = tif.read(1)
         tif_np_masked = np.ma.masked_array(tif_np, tif_np == 32767)
-        result = tif_np_masked[(col, row)]
 
-        return result
+        results = []
+        for rc in rc_list:
+            try:
+                result = tif_np_masked[rc]
+                results.append(result)
+            except IndexError:
+                print('No raster value for this pixel/date')
+                results.append(np.nan)
+    results = np.ma.filled(results, fill_value=np.nan)
+    return results
 
 
-def draw_plot(year, year_smpl_cmb_df, fig_dir, prdct, sites_dict):
+def draw_plot(year, smpl_results_df, fig_dir, prdct, sites_dict):
     sns.set_style('darkgrid')
+    smpl_results_df.rename(columns={0: 'id_0', 1: 'id_1', 2: 'id_2', 3: 'id_3', 4: 'id_4'}, inplace=True)
 
-    for site in sites_dict.keys():
-        for sds in ['wsa', 'bsa']:
-            col_name = str(site) + '_' + str(sds)
+    for site in smpl_results_df.columns.tolist():
+        if site != 'doy':
 
+            print(site)
             # Create a seaborn scatterplot (or replot for now, small differences)
-            sct = sns.regplot(x='doy', y=col_name, data=year_smpl_cmb_df, marker='o', label='sw ' + str(sds),
+            #sct = sns.scatterplot(x='doy', y=site, data=smpl_results_df)
+            sct = sns.regplot(x='doy', y=site, data=smpl_results_df, marker='o', label='sw ' ,
                               fit_reg=False, scatter_kws={'color':'darkblue', 'alpha':0.3,'s':20})
             sct.set_ylim(0, 1.0)
             sct.set_xlim(1, 366)
             sct.legend(loc='best')
 
             # Access the figure, add title
-            plt_name = str(year + ' ' + prdct + ' SW ' + str(sds))
+            plt_name = str(year + ' ' + prdct )
             plt.title(plt_name)
             #plt.show()
 
@@ -129,8 +135,16 @@ def main():
 
     # Note: I have chosen to call the landsat product LC08, rather than LC8, due to the file naming convention
     # of the inputs specific to the albedo code. LC8 is also used in different Landsat data products, annoyingly.
+    #TODO to avoid egregious geolocation errors, I should require the csv to have headers (currently it cannot)
+    #and the headers should be id,lat,long, so I can check that the latitude and longitude cols are correct by
+    #their name in the csv
     prdct = args.prdct
     base_dir = args.base_dir
+    fig_dir = os.path.join(base_dir, "fig")
+
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+
     years = [args.years]
     sites_csv_input = os.path.join(base_dir, args.sites_csv_fname)
     sites_dict = {}
@@ -139,9 +153,6 @@ def main():
         for row in reader:
             key = row[0]
             sites_dict[key] = row[1:]
-
-    # TODO this 'copy_srs_dir' location is here because currently VNP43 has broken spatial reference
-    # TODO information. Check V002 and remove this if it has been fixed, as this is ludicrously clunky.
 
     # Loop through the years provided, and extract the pixel values at the provided coordinates. Outputs CSV and figs.
     for year in years:
@@ -157,71 +168,54 @@ def main():
         # and start it off with all possible doys (366)
         year_smpl_cmb_df = pd.DataFrame(doy_list, columns=['doy'])
         # Loop through each site and extract the pixel values
-        for site in sites_dict.items():
-            in_dir = base_dir
-            fig_dir = os.path.join(base_dir, 'figs')
-            if not os.path.isdir(fig_dir):
-               os.makedirs(fig_dir)
-               print('Made new folder for figs: ' + str(fig_dir))
+
+        # Create empty array for mean
+        tif_mean = []
+        for day in doy_list:
+            # Open the ONLY BAND IN THE TIF! Cannot currently deal with multiband tifs
+            t_file_list = make_prod_list(base_dir, prdct, year, day)
+            file_name = '{in_dir}/{prdct}*{year}{day:03d}*.tif'.format(in_dir=base_dir,
+                                                                       prdct=prdct,
+                                                                       day=day,
+                                                                       year=year)
+            # See if there is a raster for the date, if not use a fill value for the graph
+            if len(t_file_list) == 0:
+                #print('File not found: ' + file_name)
+                pixel_values = [np.nan] * len(sites_dict)
+            elif len(t_file_list) > 1:
+                print('Multiple matching files found for same date! Please remove one.')
+                sys.exit()
             else:
-                pass
-            try:
-                os.chdir(in_dir)
-            except FileNotFoundError:
-                print('Sorry, data directory not found!')
-                sys.exit(1)
-            #print('Processing site: ' + str(site))
-
-            # Create empty array for mean
-            tif_mean = []
-            for day in doy_list:
-                # Open the ONLY BAND IN THE TIF! Cannot currently deal with multiband tifs
-                t_file_list = make_prod_list(in_dir, prdct, year, day)
-                file_name = '{in_dir}/{prdct}*{year}{day:03d}*.tif'.format(in_dir=in_dir, prdct=prdct, day=day,
-                                                                           year=year)
-                # See if there is a raster for the date, if not use a fill value for the graph
-                if len(t_file_list) == 0:
-                    #print('File not found: ' + file_name)
-                    pixel_value = np.nan
-                elif len(t_file_list) > 1:
-                    print('Multiple matching files found for same date! Please remove one.')
-                    sys.exit()
-                else:
-                    # print('Found file: ' + file_name)
-                    t_file_day = t_file_list[0]
-                    # Extract pixel values and append to dataframe
-                    try:
-                        extract_pixel_value(site, t_file_day)
-                    except:
-                        print('Warning! Pixel out of raster boundaries!')
-                        pixel_value = np.nan
-
-                # Add each point to a temporary list
-                pixel_value = pixel_value * 0.001
-                smpl_results = []
-                smpl_results.append(pixel_value)
+                #print('Found file: ' + file_name)
+                t_file_day = t_file_list[0]
+                # Extract pixel values and append to dataframe
                 try:
-                    tmp_mean = statistics.mean(smpl_results)
-                    tif_mean.append(tmp_mean)
+                    pixel_values = extract_pixel_values(sites_dict, t_file_day)
+                    #print(pixel_values)
                 except:
-                    tif_mean.append(np.nan)
-            cols = str((site[0][0]))
+                    print('Warning! Pixel out of raster boundaries!')
+                    pixel_values = [np.nan] * len(sites_dict)
 
-            smpl_results_df = pd.DataFrame(tif_mean, columns=[cols])
-            #print(smpl_results_df.head())
+            tif_mean.append(pixel_values)
 
-            # Append the site's results to the existing yearly dataframe, initiated above
-            year_smpl_cmb_df = pd.concat([year_smpl_cmb_df, smpl_results_df], axis=1)
+        smpl_results_df = pd.DataFrame(tif_mean)
+        smpl_results_df['doy'] = doy_list
+        cols = smpl_results_df.columns.tolist()
+        cols = cols[-1:] + cols[:-1]
+        smpl_results_df = smpl_results_df[cols]
+        smpl_results_df = smpl_results_df * 0.001
 
         # Do plotting and save output PER YEAR (individual csv per year)
-        #draw_plot(year, year_smpl_cmb_df, fig_dir, prdct, sites_dict)
+        #TODO this does not work!
+        #draw_plot(year, smpl_results_df, fig_dir, prdct, sites_dict)
 
         # Export data to csv
         os.chdir(fig_dir)
         output_name = str(sites_csv_input[:-4] + '_extracted_values')
         csv_name = str(output_name + '_' + prdct + '.csv')
-        print('writing csv: ' + csv_name)
-        year_smpl_cmb_df.to_csv(csv_name, index=False)
+        #print('writing csv: ' + csv_name)
+        #print(year_smpl_cmb_df)
+        smpl_results_df.to_csv(csv_name, index=False)
 
 
 if __name__ == '__main__':
