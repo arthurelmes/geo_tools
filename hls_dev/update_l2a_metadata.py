7ca2570a6a94f8e332ea3b6141f5855a7058e055
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-10-19 15:11:17 -0400

    Completed script to convert dates in the S2 SAFES post L2A processing.

--- hls_dev/update_l2a_metadata.py
+++ hls_dev/update_l2a_metadata.py
@@ -5,26 +5,33 @@
 import xml.etree.ElementTree as ET
 import os
 
-xml_file = "/media/arthur/Windows/LinuxShare/S2/L2A/" \
-           "S2A_MSIL2A_20200502T213531_N9999_R086_T05WPM_20201002T225054.SAFE/GRANULE/" \
-           "L2A_T05WPM_A025394_20200502T213532/MTD_TL.xml"
 
-os.chdir('/media/arthur/Windows/LinuxShare/S2/L2A/'
-         'S2A_MSIL2A_20200502T213531_N9999_R086_T05WPM_20201002T225054.SAFE/GRANULE/')
+def update_date(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    print("updating: {x}".format(x=xml_file))
+    for item in root.findall('*'):
+        for child in item:
+            if child.tag == 'L1C_TILE_ID':
+                correct_date = child.text.split("_")[6]
+            elif child.tag == 'TILE_ID':
+                incorrect_date = str(child.text.split("_")[6])
+                id_beginning = '_'.join(child.text.split("_")[:6])
+                id_end = '_'.join(child.text.split("_")[7:])
+                print(incorrect_date)
+                print(correct_date)
 
-tree = ET.parse(xml_file)
-root = tree.getroot()
+                child.text = id_beginning + "_" + correct_date + "_" + id_end
+
+                print(id_beginning + "_" + correct_date + "_" + id_end)
+    tree.write(xml_file)
+    print()
 
 
-for item in root.findall('*'):
-    for child in item:
-        #print(child.tag)
-        if child.tag == 'L1C_TILE_ID':
-            correct_date = child.text.split("_")[7]
-        elif child.tag == 'TILE_ID':
-            incorrect_date = str(child.text.split("_")[7])
-            id_beginning = '_'.join(child.text.split("_")[:7])
-            id_end = '_'.join(child.text.split("_")[8:])
-            print(child.text)
-            child.text = id_beginning + "_" + correct_date + "_" + id_end
-            print(child.text)
+if __name__ == '__main__':
+    wk_dir = '/home/arthur/Desktop/S2_meta/'
+    for root, dirs, files in os.walk(wk_dir):
+        for file in files:
+            if file == 'MTD_TL.xml':
+                print(os.path.join(root, file))
+                update_date(os.path.join(root, file))
