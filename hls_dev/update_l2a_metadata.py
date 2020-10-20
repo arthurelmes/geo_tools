bdf5408cb1819eb0ad9dd5875a0cd2604d9766e8
arthur.elmes@gmail.com <arthur.elmes@gmail.com>
2020-10-20 09:24:25 -0400

    Fixed ANOTHER bug in update_l2a_metadata.py

--- hls_dev/update_l2a_metadata.py
+++ hls_dev/update_l2a_metadata.py
@@ -3,25 +3,30 @@
 # sentinel albedo output date, rather than the acquisition date
 
 import xml.etree.ElementTree as ET
-import os
+import os, sys
 
 
 def update_date(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     print("updating: {x}".format(x=xml_file))
+
     for item in root.findall('*'):
-        print(item.tag)
         for child in item:
+            # Why do some of the names have a __???
+            if "__" in child.text:
+                index = 7
+            else:
+                index = 6
+
             if child.tag == 'L1C_TILE_ID':
-                correct_date = child.text.split("_")[7]
-                print(correct_date)
+                correct_date = child.text.split("_")[index]
 
             if child.tag == 'TILE_ID':
-                incorrect_date = str(child.text.split("_")[7])
-                print(incorrect_date)
-                id_beginning = '_'.join(child.text.split("_")[:7])
-                id_end = '_'.join(child.text.split("_")[8:])
+                # incorrect_date = str(child.text.split("_")[index])
+                # print(incorrect_date)
+                id_beginning = '_'.join(child.text.split("_")[:index])
+                id_end = '_'.join(child.text.split("_")[index + 1:])
                 correct_name = id_beginning + "_" + correct_date + "_" + id_end
                 child.text = correct_name
 
@@ -32,7 +37,7 @@ def update_date(xml_file):
 
 
 if __name__ == '__main__':
-    wk_dir = '/home/arthur/Desktop/S2_meta/'
+    wk_dir = sys.argv[1]
     for root, dirs, files in os.walk(wk_dir):
         for file in files:
             if file == 'MTD_TL.xml':
