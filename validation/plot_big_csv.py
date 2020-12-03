48dd049600342b1516177e4dd2f2bdc1f1e96b24
Arthur Elmes <arthur.elmes@gmail.com>
2020-12-03 09:43:47 -0500

    Minor changes.

--- validation/plot_big_csv.py
+++ validation/plot_big_csv.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import os, sys
+from csv import reader
 
 
 def make_ax(x, y, ax):
@@ -50,24 +51,37 @@ def make_ax(x, y, ax):
 # Here, loop over all pairs of x,y from the different tiles, which are in
 # different CSV files
 workspace = '/data/compare_MCD_VNP_VJ1/'
-#
+
+# Get these by globbing *.csv in workspace
 csv_names = [ 'h08v05_VJ1_VNP_2019_all_SMALL.csv', 'h11v09_VJ1_VNP_2019_all.csv']
 
+# Maybe experiment with this?
 chunksize = 10 ** 6
 
 # Set up the figure first, then fill with axes
+# TODO define the number of rows based on length of csv_names
 fig, ax = plt.subplots(1, 2, figsize=(6, 3))
 fig.tight_layout(pad=3)
 
 # ax counter
 i = 0
-year = 0
+year = ''
+tile = ''
 for csv_name in csv_names:
+    # Get the header names
+    header = []
+    with open(workspace + csv_name, 'r') as read_object:
+        csv_reader = reader(read_object)
+        header = next(csv_reader)
+
+    # Pull the tile and year from filename
     tile = os.path.basename(csv_name)[:6]
     year = os.path.basename(csv_name)[7:11]
-    # TODO This dataframe should be initialized from the header in the big CSV, rather than hard-coded
-    sample = pd.DataFrame(columns=['VJ143MA3.A2019009.002', 'VNP43MA3.A2019009.002'])
 
+    # Create an empty df to add to
+    sample = pd.DataFrame(columns=header)
+
+    # The csvs are so huge we have to step through them in chunks
     for chunk in pd.read_csv(workspace + csv_name, chunksize=chunksize):
         subset = chunk.loc[np.random.choice(chunk.index, 5, replace=False)]
         sample = sample.append(subset)
@@ -80,8 +94,7 @@ for csv_name in csv_names:
     x = sample_masked[:, 0]
     y = sample_masked[:, 1]
 
-    print(i)
     make_ax(x, y, ax[i])
     i += 1
 
-fig.savefig(workspace + year + '.png')
+fig.savefig(workspace + '/test.png')
