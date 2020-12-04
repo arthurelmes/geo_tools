f4e312eecde0e5374a17a25f9a28ff859be1c288
Arthur Elmes <arthur.elmes@gmail.com>
2020-12-03 19:10:11 -0500

    Improved plot_big_csv.py and minor change to laadsweb downloader.

--- validation/plot_big_csv.py
+++ validation/plot_big_csv.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import os, sys
 from csv import reader
-
+from glob import glob
 
 def make_ax(x, y, ax):
     # This is a density scatterplot; maybe tweak bins to get the look right
@@ -44,57 +44,63 @@ def make_ax(x, y, ax):
 
     props = dict(boxstyle='round', facecolor='white', alpha=0.5)
     ax.text(0.05, 0.95, textstr, fontsize=10, verticalalignment='top', bbox=props)
-
+    # print('Made plot for {x} vs {y}'.format(x=sample.columns[0], y=sample.columns[1]))
     return hist
 
 
 # Here, loop over all pairs of x,y from the different tiles, which are in
 # different CSV files
-workspace = '/data/compare_MCD_VNP_VJ1/'
+tiles = ['h08v05', 'h09v04', 'h11v04', 'h12v04', 'h26v04', 'h30v11']
 
-# Get these by globbing *.csv in workspace
-csv_names = [ 'h08v05_VJ1_VNP_2019_all_SMALL.csv', 'h11v09_VJ1_VNP_2019_all.csv']
+workspace = '/ipswich/data02/arthur.elmes/comparo_results/csv/'
 
-# Maybe experiment with this?
-chunksize = 10 ** 6
+for tile in tiles:
+    csv_names = glob(os.path.join(workspace, tile) + '/*all.csv')
+    # Maybe experiment with this?
+    chunksize = 10 ** 6
 
-# Set up the figure first, then fill with axes
-# TODO define the number of rows based on length of csv_names
-fig, ax = plt.subplots(1, 2, figsize=(6, 3))
-fig.tight_layout(pad=3)
+    # Set up the figure first, then fill with axes
+    # TODO define the number of rows based on length of csv_names
+    fig, ax = plt.subplots(1, 3, figsize=(7, 3))
+    fig.tight_layout(pad=3)
 
-# ax counter
-i = 0
-year = ''
-tile = ''
-for csv_name in csv_names:
-    # Get the header names
-    header = []
-    with open(workspace + csv_name, 'r') as read_object:
-        csv_reader = reader(read_object)
-        header = next(csv_reader)
+    # ax counter
+    i = 0
+    year = ''
+    tile = ''
+    for csv_name in csv_names:
+        print(csv_name)
+        # Get the header names
+        header = []
+        try:
+            with open(csv_name, 'r') as read_object:
+                csv_reader = reader(read_object)
+                header = next(csv_reader)
 
-    # Pull the tile and year from filename
-    tile = os.path.basename(csv_name)[:6]
-    year = os.path.basename(csv_name)[7:11]
+            # Pull the tile and year from filename
+            tile = os.path.basename(csv_name)[:6]
+            year = os.path.basename(csv_name)[7:11]
 
-    # Create an empty df to add to
-    sample = pd.DataFrame(columns=header)
+            # Create an empty df to add to
+            sample = pd.DataFrame(columns=header)
 
-    # The csvs are so huge we have to step through them in chunks
-    for chunk in pd.read_csv(workspace + csv_name, chunksize=chunksize):
-        subset = chunk.loc[np.random.choice(chunk.index, 5, replace=False)]
-        sample = sample.append(subset)
+            # The csvs are so huge we have to step through them in chunks
+            for chunk in pd.read_csv(csv_name, chunksize=chunksize):
+                subset = chunk.loc[np.random.choice(chunk.index, 30, replace=False)]
+                sample = sample.append(subset)
 
-    # Mask out 32.767 values which should have already been removed!!
-    nodata_masked = np.ma.masked_array(sample, sample == 32.767)
-    sample_masked = np.ma.filled(nodata_masked, np.nan)
+            # Mask out 32.767 values which should have already been removed!!
+            nodata_masked = np.ma.masked_array(sample, sample == 32.767)
+            sample_masked = np.ma.filled(nodata_masked, np.nan)
 
-    # Set the x and y from the masked data
-    x = sample_masked[:, 0]
-    y = sample_masked[:, 1]
+            # Set the x and y from the masked data
+            x = sample_masked[:, 0]
+            y = sample_masked[:, 1]
 
-    make_ax(x, y, ax[i])
-    i += 1
-
-fig.savefig(workspace + '/test.png')
+            make_ax(x, y, ax[i])
+            i += 1
+        except:
+            print("File could not be opened!")
+            
+    fname = os.path.join(workspace, tile) + '/' + tile + '_2019_scatterplots.png'
+    fig.savefig(fname)
