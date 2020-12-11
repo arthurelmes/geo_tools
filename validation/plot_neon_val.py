21e1b14c337e689c9632586fe4e6e34b06ef5410
Arthur Elmes <arthur.elmes@gmail.com>
2020-12-11 09:06:07 -0500

    Made new general purpose py file to become importable module later, with doy converter function to start off. Updated NEON plotting script, which should eventually be merged with the other two in situ plotting tools.

--- validation/plot_neon_val.py
+++ validation/plot_neon_val.py
@@ -1,9 +1,9 @@
-""" This script plots SURFRAD albedo against extracted satellite data (use ../time_series/extract_samples_modisviirs43.py)
- and the pre-exisiting IDL SURFRAD formatting code (readgroundmeasurement_surfrad.pro and calaveragep.pro, currently
+""" This script plots NEON albedo against extracted satellite data (use ../time_series/extract_samples_modisviirs43.py)
+ and the pre-exisiting IDL NEON formatting code (readgroundmeasurement_surfrad.pro and calaveragep.pro, currently
  in Arthur's neponset homedir: /home/arthur.elmes/code/val_data/). These format data correctly for ingestion into
  this script.
  Author: Arthur Elmes
- Date: 2020-12-4
+ Date: 2020-12-11
 """
 
 import matplotlib.pyplot as plt
@@ -15,14 +15,12 @@ import os, sys, csv
 #TODO change to CLI arg
 workspace = '/home/arthur/Dropbox/projects/modis_viirs_continuity/in_situ/'
 
-# SURFRAD data
-
-# These should be the the SURFRAD standard abbreviations
-stn_list = ['drk', 'tbl', 'ftp', 'sxf']
+# These should be the the NEON standard abbreviations
+stn_list = ['HARV']
 
 # make csv to hold stats
-stats_fields = ['stn', 'rmse_mcd_sfrd', 'mb_mcd_sfrd', 'rmse_vnp_sfrd', 'mb_vnp_sfrd', 'rmse_vj1_sfrd', 'mb_vj1_sfrd']
-csv_fname = workspace + '/sfrd_stats.csv'
+stats_fields = ['stn', 'rmse_mcd_neon', 'mb_mcd_neon', 'rmse_vnp_neon', 'mb_vnp_neon', 'rmse_vj1_neon', 'mb_vj1_neon']
+csv_fname = workspace + '/neon_stats.csv'
 
 with open(csv_fname, 'w') as csvfile:
     # creating a csv writer object
@@ -33,27 +31,31 @@ with open(csv_fname, 'w') as csvfile:
 
 
 for stn in stn_list:
-    os.chdir(os.path.join(workspace, 'surfrad'))
-    sfrd = pd.read_csv(stn + '_2019.csv')
+    os.chdir(os.path.join(workspace, 'neon'))
+    neon = pd.read_csv(stn + '_2019.csv', index_col=0, parse_dates=True)
+    neon.reset_index(inplace=True)
 
     # Extracted data from MODIS/VIIRS
     mcd = pd.read_csv(os.path.join(workspace, 'extracted', 'mcd')
-                      + '/surfrad_sites_subset_formatted_extracted_values_MCD43A3.csv',
+                      + '/hf_neon_tower_extracted_values_MCD43A3.csv',
                       index_col=0)
     vnp = pd.read_csv(os.path.join(workspace, 'extracted', 'vnp')
-                      + '/surfrad_sites_subset_formatted_extracted_values_VNP43MA3.csv',
+                      + '/hf_neon_tower_extracted_values_VNP43MA3.csv',
                       index_col=0)
     vj1 = pd.read_csv(os.path.join(workspace, 'extracted', 'vj1')
-                      + '/surfrad_sites_subset_formatted_extracted_values_VJ143MA3.csv',
+                      + '/hf_neon_tower_extracted_values_VJ143MA3.csv',
                       index_col=0)
-    wsa = stn + '_wsa'
-    bsa = stn + '_bsa'
+    # wsa = stn + '_wsa'
+    # bsa = stn + '_bsa'
+    wsa = '1_wsa'
+    bsa = '1_bsa'
     wsa_mcd = stn + '_wsa_mcd'
     bsa_mcd = stn + '_bsa_mcd'
     wsa_vnp = stn + '_wsa_vnp'
     bsa_vnp = stn + '_bsa_vnp'
     wsa_vj1 = stn + '_wsa_vj1'
     bsa_vj1 = stn + '_bsa_vj1'
+
 
     # Make the WSA variables distinguishable
     mcd.rename(columns={wsa: wsa_mcd,
@@ -64,35 +66,36 @@ for stn in stn_list:
                         bsa: bsa_vj1}, inplace=True)
 
     # Convert the individual data fields to datetime field
-    sfrd['Date'] = pd.to_datetime(sfrd['year'].map(str) + '-' + sfrd['month'].map(str) + '-' + sfrd['day'].map(str))
-    sfrd['doy'] = sfrd['Date'].dt.dayofyear
+    #neon['Date'] = pd.to_datetime(neon['year'].map(str) + '-' + neon['month'].map(str) + '-' + neon['day'].map(str))
+    neon['doy'] = neon['date'].dt.dayofyear
 
     # Then get julian date from it, to use as a join field
-    sfrd.set_index('doy', inplace=True)
-    sfrd.drop(columns=['month', 'day', 'year', 'gnd_sdev', 'dir/dif_ratio', 'diffuse/(dir+diff)', 'zen', 'Date'],
+    neon.set_index('doy', inplace=True)
+    neon.drop(columns=['date'],
               axis=1, inplace=True)
 
     # This plots the set of scatterlpots against the SURFRAD (x) and satellite (y)
     #TODO this whole section should automatically have the number of figs as input datasets, e.g. just MCD, or MCD and VNP, etc
-    sfrd = sfrd.join(mcd,  lsuffix='_sfrd', rsuffix='_snsr')
-    sfrd = sfrd.join(vnp,  lsuffix='_sfrd', rsuffix='_snsr')
-    sfrd = sfrd.join(vj1,  lsuffix='_sfrd', rsuffix='_snsr')
+    neon = neon.join(mcd,  lsuffix='_neon', rsuffix='_snsr')
+    neon = neon.join(vnp,  lsuffix='_neon', rsuffix='_snsr')
+    neon = neon.join(vj1,  lsuffix='_neon', rsuffix='_snsr')
 
-    stn_wsa_col = sfrd['gnd_mean']
-    mcd = sfrd[stn + '_wsa_mcd']
-    vnp = sfrd[stn + '_wsa_vnp']
-    vj1 = sfrd[stn + '_wsa_vj1']
+
+    stn_wsa_col = neon['albedo']
+    mcd = neon[stn + '_wsa_mcd']
+    vnp = neon[stn + '_wsa_vnp']
+    vj1 = neon[stn + '_wsa_vj1']
 
     #TODO this should obviously be a function
     # Calculate RMSE and Mean Bias
-    rmse_mcd_sfrd = ((stn_wsa_col - mcd) ** 2).mean() ** 0.5
-    mb_mcd_sfrd = stn_wsa_col.mean() - mcd.mean()
-    rmse_vnp_sfrd = ((stn_wsa_col - vnp) ** 2).mean() ** 0.5
-    mb_vnp_sfrd = stn_wsa_col.mean() - vnp.mean()
-    rmse_vj1_sfrd = ((stn_wsa_col - vj1) ** 2).mean() ** 0.5
-    mb_vj1_sfrd = stn_wsa_col.mean() - vj1.mean()
+    rmse_mcd_neon = ((stn_wsa_col - mcd) ** 2).mean() ** 0.5
+    mb_mcd_neon = stn_wsa_col.mean() - mcd.mean()
+    rmse_vnp_neon = ((stn_wsa_col - vnp) ** 2).mean() ** 0.5
+    mb_vnp_neon = stn_wsa_col.mean() - vnp.mean()
+    rmse_vj1_neon = ((stn_wsa_col - vj1) ** 2).mean() ** 0.5
+    mb_vj1_neon = stn_wsa_col.mean() - vj1.mean()
 
-    stats_row = [stn, rmse_mcd_sfrd, mb_mcd_sfrd, rmse_vnp_sfrd, mb_vnp_sfrd, rmse_vj1_sfrd, mb_vj1_sfrd]
+    stats_row = [stn, rmse_mcd_neon, mb_mcd_neon, rmse_vnp_neon, mb_vnp_neon, rmse_vj1_neon, mb_vj1_neon]
     with open(csv_fname, 'a+', newline='') as write_obj:
         csv_writer = csv.writer(write_obj)
         csv_writer.writerow(stats_row)
@@ -115,21 +118,15 @@ for stn in stn_list:
     plt.show()
 
     # This plots the satellite and SURFRAD data against DOY, so a time series
-    sfrd.reset_index(inplace=True)
-    doy = sfrd['doy']
+    neon.reset_index(inplace=True)
+    doy = neon['doy']
 
     # Slapdash conversion between stn abbr and full namm
     #TODO this should be done with a dictionary instead, way above, that is iterated over
 
-    ['drk', 'tbl', 'ftp', 'sxf']
-    if stn == 'drk':
-        stn_full = 'Desert Rock, NV'
-    elif stn == 'tbl':
-        stn_full = 'Table Mountain, CO'
-    elif stn == 'ftp':
-        stn_full = 'Fort Peck, MT'
-    elif stn == 'sxf':
-        stn_full = 'Sioux Falls, SD'
+
+    if stn == 'HARV':
+        stn_full = 'Harvard Forest, MA'
     else:
         print('Station name error!')
         sys.exit(1)
@@ -151,8 +148,8 @@ for stn in stn_list:
     ax.tick_params(axis='y', labelsize=12)
 
     ax.legend(prop={'size': 12})
-    # txt_rmse = 'RMSE=' + str(round(rmse_mcd_sfrd, 4))
-    # txt_mb = 'MB = ' + str(round(mb_mcd_sfrd, 4))
+    # txt_rmse = 'RMSE=' + str(round(rmse_mcd_neon, 4))
+    # txt_mb = 'MB = ' + str(round(mb_mcd_neon, 4))
     # fig2.text(0.8, 0.85, txt_rmse, color='white')
     plt.savefig(workspace + stn + '_timeseries.png')
     plt.show()
