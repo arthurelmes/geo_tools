2ba3b88697f642aa05136061f7660b1c6f25e70c
Arthur Elmes <arthur.elmes@gmail.com>
2020-12-16 08:14:27 -0500

    Minor chagnes to make_gif.py

--- time_series/make_gif.py
+++ time_series/make_gif.py
@@ -3,17 +3,27 @@ import os, sys
 
 comparos = ['MCD_VNP', 'MCD_VJ1', 'VJ1_VNP']
 tiles = ['h08v05', 'h09v04', 'h11v04', 'h11v09', 'h12v04', 'h16v02', 'h26v04', 'h30v11']
+m_bands = ['Band1', 'Band2', 'Band3', 'Band4', 'Band5', 'Band6', 'Band7', 'nir', 'shortwave', 'vis']
+v_bands = ['M3', 'M4', 'M5', 'M7', 'M8', 'M10', 'M11', 'nir', 'shortwave', 'vis']
 
-for comparo in comparos:
-    for tile in tiles:
-        png_dir = '/data/mcd_vj1_vnp_comparo/' + comparo + '/' + tile + '/'
-        gif_dir = '/home/arthur/Dropbox/projects/modis_viirs_continuity/sensor_intercompare/'
+for m_band in m_bands:
+    for comparo in comparos:
+        for tile in tiles:
+            png_dir = os.path.join('/data/mcd_vj1_vnp_comparo/', comparo, tile, m_band)
+            gif_dir = '/home/arthur/Dropbox/projects/modis_viirs_continuity/sensor_intercompare/'
+            gif_list = [file for file in os.listdir(png_dir) if file.endswith('.png')]
+            gif_list.sort()
+            images = []
+            for file_name in gif_list: images.append(imageio.imread(png_dir + file_name))
+            imageio.mimsave(gif_dir + comparo + '_' + tile + '.gif', images, 'GIF', duration=0.1)
 
-        gif_list = [file for file in os.listdir(png_dir) if file.endswith('.png')]
-        gif_list.sort()
-
-        images = []
-
-        for file_name in gif_list: images.append(imageio.imread(png_dir + file_name))
-
-        imageio.mimsave(gif_dir + comparo + '_' + tile + '.gif', images, 'GIF', duration=0.1)
+for v_band in v_bands:
+    for comparo in comparos:
+        for tile in tiles:
+            png_dir = os.path.join('/data/mcd_vj1_vnp_comparo/', comparo, tile, v_band)
+            gif_dir = '/home/arthur/Dropbox/projects/modis_viirs_continuity/sensor_intercompare/'
+            gif_list = [file for file in os.listdir(png_dir) if file.endswith('.png')]
+            gif_list.sort()
+            images = []
+            for file_name in gif_list: images.append(imageio.imread(png_dir + file_name))
+            imageio.mimsave(gif_dir + comparo + '_' + tile + '.gif', images, 'GIF', duration=0.1)
